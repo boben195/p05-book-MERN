1d02c395039dfc0cbcd0c0865e0cc647fcec6c6e
boben195 <boben195@ukr.net>
2025-07-16 12:05:27 +0300

    add func for data

--- frontend/src/components/ReservForm.jsx
+++ frontend/src/components/ReservForm.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const ReservForm = () => {
+  const generateTimsSlot = () => {
+    const slots = [];
+    for (let hour = 9; hour < 21; hour++) {
+      const startHour = hour % 12 === 0 ? 12 : hour % 12;
+      const startPeriod = hour < 12 ? "AM" : "PM";
+
+      const endHour = (hour + 1) % 12 === 0 ? 12 : (hour + 1) % 12;
+      const endPeriod = hour + 1 < 12 ? "AM" : "PM";
+
+      slots.push(
+        `${startHour}:00 ${startPeriod} - ${endHour}: 00 ${endPeriod}`
+      );
+    }
+    return slots;
+  };
   return (
     <div>
       <form>
@@ -11,6 +26,11 @@ const ReservForm = () => {
         <input type="date" required lang="en" />
         <select required>
           <option value="">Select Time</option>
+          {generateTimsSlot().map((slot, index) => (
+            <option key={index} value={slot}>
+              {slot}
+            </option>
+          ))}
         </select>
         <select required>
           {[
